e1964cd97629bcf9df0cd098097fd6db9f048c93
Alex <j201.alex@gmail.com>
2014-10-02 23:36:08 -0400

    Start implementing

--- lib.js
+++ lib.js
@@ -1,3 +1,5 @@
+function concat(arr, el) { return arr.concat([el]); }
+
 // Spec input:
 // get :: (TData, TEnter[]) => TValue
 // set :: (TData, TEnter[], TValue) => TData
@@ -8,5 +10,24 @@
 // set :: TValue => void
 module.exports = function(get, set) {
 	return function(store) {
+		function cursor(entries) {
+			var getCached = false;
+			var getCache;
+			return {
+				enter: function(entry) {
+					return cursor(concat(entries, entry));
+				},
+				get: function() {
+					if (getEvaluated) return getCache;
+					getCached = true;
+					getCache = get(store, entries);
+					return getCache;
+				},
+				set: function(val) {
+					getCached = false;
+					store = set(store, entries, val);
+				}
+			};
+		}
 	};
 };
