5a2b97cce3c3784115fb1d4cf4bb6d95cd2e79ad
Alex <j201.alex@gmail.com>
2014-10-16 23:30:26 -0400

    Update mori tests

--- specs/test/mori.js
+++ specs/test/mori.js
@@ -1,67 +1,63 @@
 var tape = require('tape');
-var cursor = require('../obj');
-var clone = require('clone');
+var cursor = require('../mori');
+var M = require('mori');
 
 tape("Root cursor", function(t) {
-	var obj = {a: 1, b: {c: 2}};
-	var objClone = clone(obj);
-	var objCursor = cursor(obj);
+	var map = M.hash_map("a", 1, "b", M.hash_map("c", 2));
+	var mapCursor = cursor(map);
 
-	t.deepEquals(objCursor.get(), obj, "get");
+	t.deepEquals(mapCursor.get(), map, "get");
 
-	var newObj = {a: 2, c: {d: 3}};
-	objCursor.set(newObj);
-	t.deepEquals(objCursor.get(), newObj, "set");
-	t.deepEquals(obj, objClone, "set doesn't mutate original");
+	var newMap = M.hash_map("a", 2, "c", M.hash_map("d", 3));
+	mapCursor.set(newMap);
+	t.ok(M.equals(mapCursor.get(), newMap), "set");
 
-	objCursor.transact(function(obj) {
-		var result = clone(obj);
-		result.c.d = 4;
-		return result;
+	mapCursor.transact(function(map) {
+		return M.assoc_in(map, ["c", "d"], 4);
 	});
-	t.deepEquals(objCursor.get(), {a: 2, c: {d: 4}}, "transact");
+	t.ok(M.equals(mapCursor.get(), M.hash_map("a", 2, "c", M.hash_map("d", 4))), "transact");
 
 	var unique = {};
 	var onChangeVal = unique;
-	objCursor.onChange(function(val) {
+	mapCursor.onChange(function(val) {
 		onChangeVal = val;
 	});
 	t.is(onChangeVal, unique, "onChange not called immediately");
-	objCursor.set(obj);
-	t.is(onChangeVal.get(), obj, "onChange returns a cursor of the new value");
+	mapCursor.set(map);
+	t.is(onChangeVal.get(), map, "onChange returns a cursor of the new value");
 
 	t.end();
 });
 
 tape("Sub-cursor", function(t) {
-	var obj = {a: 1, b: {c: {d: 2}}};
-	var objClone = clone(obj);
-	var objCursor = cursor(obj);
-	var subCursor = objCursor.enter('b', 'c');
+	var map = {a: 1, b: {c: {d: 2}}};
+	var mapClone = clone(map);
+	var mapCursor = cursor(map);
+	var subCursor = mapCursor.enter('b', 'c');
 
 	t.deepEquals(subCursor.get(), {d: 2}, "enter and get");
 
 	subCursor.set({e: 3});
 	t.deepEquals(subCursor.get(), {e: 3}, "set");
-	t.deepEquals(objCursor.get(), {a: 1, b: {c: {e: 3}}}, "set propagates to parent");
-	t.deepEquals(obj, objClone, "set doesn't mutate original");
+	t.deepEquals(mapCursor.get(), {a: 1, b: {c: {e: 3}}}, "set propagates to parent");
+	t.deepEquals(map, mapClone, "set doesn't mutate original");
 
-	subCursor.transact(function(obj) {
-		var result = clone(obj);
+	subCursor.transact(function(map) {
+		var result = clone(map);
 		result.f = 4;
 		return result;
 	});
 	t.deepEquals(subCursor.get(), {e: 3, f: 4}, "transact");
-	t.deepEquals(objCursor.get(), {a: 1, b: {c: {e: 3, f: 4}}}, "transact propagates to parent");
+	t.deepEquals(mapCursor.get(), {a: 1, b: {c: {e: 3, f: 4}}}, "transact propagates to parent");
 
 	var unique = {};
 	var onChangeVal = unique;
-	objCursor.onChange(function(val) {
+	mapCursor.onChange(function(val) {
 		onChangeVal = val;
 	});
 	t.is(onChangeVal, unique, "onChange not called immediately");
 	subCursor.set({d: 2});
-	t.deepEquals(onChangeVal.get(), obj, "Subcursor changes cause onChange listeners on root to be called");
+	t.deepEquals(onChangeVal.get(), map, "Subcursor changes cause onChange listeners on root to be called");
 
 	t.end();
 });
